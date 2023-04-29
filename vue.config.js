cf5736bdbee144a5f286229cdd93c7e0747a7647
OlivierC <coq.olivier36@gmail.com>
2023-04-29 09:30:40 -0400

    Rebuilding yet again. Please kill me.

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV  ===  'production'  ?  './'  :  '/',
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: `@import "@/sass/main.scss";`
-      }
-    }
-  }
+  transpileDependencies: true
 })
