6adacd8e7897a419a6ac876720104d142701f6cf
andrew <andrewscaldwell@gmail.com>
2023-06-04 14:26:49 -0400

    added note tabs

---
+++ local_files/readme.md
@@ -0,0 +1,1 @@
+files are located here
---
+++ src/components/NoteTabs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-tabs v-model="activeNote" fixed-tabs>
+            <v-tab>Active Tab</v-tab>
+            <v-tab>Second Tab</v-tab>
+            <v-tab>Third Tab</v-tab>
+        </v-tabs>
+        <v-divider></v-divider>
+    </div>
+</template>
+
+<script>
+
+import { reactive, toRefs } from 'vue'
+
+export default {
+    setup () {
+        const state = reactive({
+            activeNote: 0,
+        })
+    
+        return {
+            ...toRefs(state),
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
