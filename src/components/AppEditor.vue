02ec9cd734904bc45671751f0ba6e7ef0b7133a0
andrew <andrewscaldwell@gmail.com>
2023-06-04 14:47:03 -0400

    note content editable

---
+++ src/components/AppEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <editor-content :editor="editor" class="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+export default {
+  setup() {
+    const editor = new Editor({
+      extensions: [StarterKit],
+      content: `<p>You've created a new note with Stack Notes 🎉 Start writing code here 👨‍💻👩‍💻</p>`,
+    });
+
+    return {
+      editor,
+    };
+  },
+  components: {
+    EditorContent,
+  },
+};
+</script>
+
+<style lang="scss">
+.ProseMirror {
+  border: none;
+  overflow: auto;
+  outline: none;
+
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+
+  resize: none; /*remove the resize handle on the bottom right*/
+}
+</style>
--- src/components/MainUI.vue
+++ src/components/MainUI.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="bg-grey-darken-4 fill-height">
     <note-tabs />
+    <div class="pa-5">
+      <app-editor class="fill-height" />
+    </div>
   </div>
 </template>
 <script>
 import NoteTabs from "./NoteTabs.vue";
+import AppEditor from "./AppEditor.vue";
 export default {
   components: {
     NoteTabs,
+    AppEditor,
   },
 };
-
 </script>
 <style lang="scss">
 #main-ui {
