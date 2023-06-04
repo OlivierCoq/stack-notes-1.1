161752c34010bf94f306c8c293687419436cef2a
OlivierC <coq.olivier36@gmail.com>
2023-06-04 14:28:56 -0400

    Removing breaking vuetify conflict

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { createPinia } from 'pinia'
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(createPinia()).use(vuetify).mount('#app')
